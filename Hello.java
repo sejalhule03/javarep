cb94e6c454eb7f1eae31a1fc9f90ca82a9007700
sejalhule03 <sejalhule03@gmail.com>
2026-03-23 13:57:28 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello sejal, World!");
+        System.out.println("Hello from GitHub - Poll SCM Working");
     }
 }
